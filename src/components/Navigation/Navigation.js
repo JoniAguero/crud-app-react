30dec5838e91c4ba99f5691770034ebf225c40a2
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-24 20:49:49 -0300

    Modificaciones Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,16 +5,12 @@ const Navigation = () => {
 
     return (
         <nav className="row top">
-            <a className="col-6">
-                <Link to={'/posts'}>
-                    <h2 className="text-center"> All Post </h2>
-                </Link>
-            </a>
-            <a className="col-6">
-                <Link to={'/new'}>
-                    <h2 className="text-center"> New Post </h2>
-                </Link>
-            </a>
+            <Link to={'/posts'} className="col-6">
+                <h2 className="text-center"> All Post </h2>
+            </Link>
+            <Link to={'/new'} className="col-6"> 
+                <h2 className="text-center"> New Post </h2>
+            </Link>
         </nav>
     )
 }
