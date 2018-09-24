a0ff0f5e3c4aa3f56fa09c274fbdf86448790179
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-24 20:49:40 -0300

    Get Posts

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,19 +5,36 @@ import Header from './Header/Header';
 import Navigation from './Navigation/Navigation';
 
 export class Router extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-            <div>
-                <Header />
-                <Navigation />
-            </div>
-            {/* <Route /> */}
-        </Switch>
-      </BrowserRouter>
-    )
-  }
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        this.obtenerPosts();
+    }
+
+    obtenerPosts = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then( res => {
+            this.setState({
+                posts: res.data
+            })
+        })
+    }
+
+    render() {
+        return (
+        <BrowserRouter>
+            <Switch>
+                <div>
+                    <Header />
+                    <Navigation />
+                </div>
+                {/* <Route /> */}
+            </Switch>
+        </BrowserRouter>
+        )
+    }
 }
 
 export default Router;
