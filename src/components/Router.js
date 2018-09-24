311ec4e68f6d4b69a4f952ab59984ee02df5e185
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-24 19:31:12 -0300

    Initial + Router

---
+++ src/components/Router.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Router extends Component {
+  render() {
+    return (
+      <div>
+        desde router
+      </div>
+    )
+  }
+}
+
+export default Router;
