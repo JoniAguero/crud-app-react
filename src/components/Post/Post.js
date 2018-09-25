503fe8a293dbc9799df38e7db67c8552cb809d70
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-24 21:32:17 -0300

    Post

---
+++ src/components/Post/Post.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+export class Post extends Component {
+  render() {
+
+    const {id, title} = this.props.info;
+
+    return (
+      <tr>
+        <td>{id}</td>
+        <td>{title}</td>
+        <td>
+            <Link to={`/post/${id}`} className="btn btn-primary">Ver</Link>
+            <button to={`/post/${id}`} className="btn btn-success">Editar</button>
+            <button to={`/post/${id}`} className="btn btn-danger">Eliminar</button>
+        </td>
+      </tr>
+    )
+  }
+}
+
+export default Post;
